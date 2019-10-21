264d8eb82c77c07de17d27d9cca91fba709d298a
alanhenschel <alanhenschel@pop-os.localdomain>
2019-10-21 17:54:55 -0300

    muitos para muitos

--- src/main/java/com/alan/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/alan/cursomc/resources/CategoriaResource.java
@@ -17,6 +17,7 @@ public class CategoriaResource {
 	
 	@Autowired
 	private CategoriaService service;
+	
 	@RequestMapping(value="/{id}",method=RequestMethod.GET)
 	public ResponseEntity<?> find(@PathVariable Integer id) {
 		Categoria obj = service.find(id);
